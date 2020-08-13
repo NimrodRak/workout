94340fc0add99de554c01a1539ec26f2f963f753
Michal Rak <michali.rak@gmail.com>
2020-08-13 13:20:31 +0300

    Changed file format and fixed backend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for
 from json import dumps
 import os
+
 app = Flask(__name__)
 
 
@@ -8,32 +9,26 @@ app = Flask(__name__)
 def index():
     workouts_list = []
     for file_name in os.listdir("static/workouts"):
-        with open("static/workouts/" + file_name) as file:
-            workouts_list.append(file.readline().replace('\n', ''))
-
+        workouts_list.append(file_name)
     return render_template("index.html", options=workouts_list)
 
 
 @app.route('/workout')
 def workout():
-
     workout = request.args.get("name")
+    # TODO: Check input to avoid security bugs
 
-    for file_name in os.listdir("static/workouts"):
-        with open("./static/workouts/" + file_name) as file:
-            name = file.readline().replace("\n", "")
-            if name == workout:
-                workout = file_name
-                break
+    if not os.path.isfile(f"static/workouts/{workout}.csv"):
+        # TODO: Handle error
+        pass
 
     with open(f'static/workouts/{workout}') as file:
-        name = file.readline().replace('\n', '')
         exercise_list = []
         for line in file:
             splitted = line.replace('\n', '').split(", ")
             exercise_list.append({"name": splitted[0].capitalize(), "ex_time": int(splitted[1]) * 1000, "br_time": int(splitted[2]) * 1000})
 
-    return render_template("workout.html", exercises_list=dumps(exercise_list), name=name)
+    return render_template("workout.html", exercises_list=dumps(exercise_list), name=workout)
 
 
 if __name__ == '__main__':
