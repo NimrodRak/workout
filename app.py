8eedc457d99787b434248c6a182a4309a0ac1328
Michal Rak <michali.rak@gmail.com>
2020-08-13 14:49:55 +0300

    Unified quotation mark usage

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 from json import dumps
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     workouts_list = []
     for file_name in os.listdir("static/workouts"):
-        workouts_list.append(file_name.strip('.csv'))
+        workouts_list.append(file_name.strip(".csv"))
     return render_template("index.html", options=workouts_list)
 
 
-@app.route('/workout')
+@app.route("/workout")
 def workout():
-    workout = request.args.get("name")
+    workout = request.args.get("workout_name")
     # TODO: Check input to avoid security bugs
 
     if not os.path.isfile(f"static/workouts/{workout}.csv"):
         # TODO: Handle error
         pass
 
-    with open(f'static/workouts/{workout}.csv') as file:
+    with open("static/workouts/{workout}.csv") as file:
         exercise_list = []
         # TODO: parse as CSV
         for line in file:
-            splitted = line.replace('\n', '').split(", ")
+            splitted = line.replace("\n", "").split(", ")
             exercise_list.append({"name": splitted[0].capitalize(), "ex_time": int(splitted[1]) * 1000, "br_time": int(splitted[2]) * 1000})
 
     return render_template("workout.html", exercises_list=dumps(exercise_list), name=workout)
